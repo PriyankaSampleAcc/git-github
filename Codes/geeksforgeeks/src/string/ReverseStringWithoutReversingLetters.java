72cb2890abf4282473cc6d5953a7ce2c5917678e
Priyanka Patil <priyanka.patil@contentserv.com>
2020-05-17 18:48:52 +0530

    Reverse String without reversing letters Try it Priyanka Patil

--- Codes/geeksforgeeks/src/string/ReverseStringWithoutReversingLetters.java
+++ Codes/geeksforgeeks/src/string/ReverseStringWithoutReversingLetters.java
@@ -1,5 +1,21 @@
+
 package string;
+
+import java.util.Scanner;
 
 public class ReverseStringWithoutReversingLetters {
 
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		System.out.println("Enter any string separated by dot(.)");
+		String testString = sc.nextLine();
+		String[] array = testString.split(" ");
+		String ans = "";
+		for (int i = array.length - 1; i >= 0; i--) {
+			ans = ans + array[i] + " ";
+		}
+		System.out.println(ans);
+	}
 }
