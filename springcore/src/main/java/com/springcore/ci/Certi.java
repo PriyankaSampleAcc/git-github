97516e6f41b414a596bb4c3ad9316ce99c3dc447
Priyanka Patil <priyanka.patil@contentserv.com>
2020-10-21 17:28:19 +0530

    Constructor Injection and Reference objects implementation

---
+++ springcore/src/main/java/com/springcore/ci/Certi.java
@@ -0,0 +1,20 @@
+package com.springcore.ci;
+
+public class Certi {
+	String name;
+
+	public Certi() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Certi(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Certi [name=" + name + "]";
+	}
+}
