a0f9750d14df7414b874fd795bd30e96ed1809fd
Priyanka Patil <priyanka.patil@contentserv.com>
2020-10-28 15:40:52 +0530

    Insert into Database and Update into Database

--- spring jdbc/com.springjdbc/src/main/java/com/spring/jdbc/springjdbc/App.java
+++ spring jdbc/com.springjdbc/src/main/java/com/spring/jdbc/springjdbc/App.java
@@ -4,6 +4,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.spring.jdbc.springjdbc.entities.StudentDao;
+
 /**
  * Hello world!
  *
@@ -12,13 +14,20 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Started ....");
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/springjdbc/config.xml");
-		JdbcTemplate template = context.getBean("jdbcTemplate", JdbcTemplate.class);
-		System.out.println("******");
-		// insert query
-		 String query = "insert into student(id,name,city) values(?,?,?)";
-		
-		 // fire query
-		 int result = template.update(query, 1231, "Kumar", "pune");
-		 System.out.println("No.of record inserted " + result);
+		StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
+		Student student = new Student();
+		student.setId(155);
+		student.setName("Vishal");
+		student.setCity("JAbalpur");
+		int result = studentDao.insert(student);
+		System.out.println("Rows inserted " + result);
+
+		Student student1 = new Student();
+		student1.setId(1231);
+		student1.setName("qwert");
+		student1.setCity("erty");
+		int r = studentDao.change(student);
+		System.out.println("Updated rows " + r);
+
 	}
 }
--- spring jdbc/com.springjdbc/src/main/java/com/spring/jdbc/springjdbc/dao/StudentDaoImpl.java
+++ spring jdbc/com.springjdbc/src/main/java/com/spring/jdbc/springjdbc/dao/StudentDaoImpl.java
@@ -14,12 +14,18 @@ public class StudentDaoImpl implements StudentDao {
 	}
 
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate; 
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
 	public int insert(Student student) {
 		String query = "insert into student(id,name,city) values(?,?,?)";
 		int result = this.jdbcTemplate.update(query, student.getId(), student.getName(), student.getCity());
+		return result;
+	}
+
+	public int change(Student student) {
+		String query = "update student set name = ? , city = ? where id = ?";
+		int result = this.jdbcTemplate.update(query, student.getName(), student.getCity(), student.getId());
 		return result;
 	}
 }
