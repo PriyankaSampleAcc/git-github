715aff37f59a667516025b7a098ca3b117651a81
Priyanka Patil <priyanka.patil@contentserv.com>
2020-10-30 17:40:14 +0530

    Printing date and time using service method- very basic

--- servlets/src/com/servlet/FirstServlet.java
+++ servlets/src/com/servlet/FirstServlet.java
@@ -1,6 +1,8 @@
 package com.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 import javax.servlet.*;
 
@@ -15,6 +17,11 @@ public class FirstServlet implements Servlet {
 
 	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
 		System.out.println("Servicing ..");
+		// set the content type of the response
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>This is my output from servlet program </h1>");
+		out.println("<h1>Today's date and time" + new Date().toString() + "</h1>");
 	}
 
 	public void destroy() {
